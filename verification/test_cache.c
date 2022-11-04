592ae96b88a535f2d217113caddee91372bd95bb
gagan-aryan <gagan@iitk.ac.in>
2022-11-04 23:28:34 +0530

    add: verification code

---
+++ verification/test_cache.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#define ll long long
+
+// 2 core 
+// L1D : 64 x 12 x 64 = 48 KB
+// L2D : 512 KB
+// LLC slice 1 MB size : 2^20 
+// block size : 64 bytes : 8 long long 
+
+//#define ASIZE 512*(1<<10)
+#define ASIZE 512*(1<<10)
+
+int main(){
+//    ll* A = (ll*) malloc(ASIZE*sizeof(ll));
+                          //
+    for(int i = 0; i < ASIZE ; i++){
+//	if (i%16 == 0)
+//	        A[i] = 5 * i;
+    }
+    return 0;
+}
